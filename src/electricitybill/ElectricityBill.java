0b9afcd8acef0ac7eb6818e4cdf35fcd99f164a3
ClementMathew <clementmathew924@gmail.com>
2023-03-17 01:57:37 +0530

    Bill Generator

---
+++ src/electricitybill/ElectricityBill.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package electricitybill;
+
+/**
+ *
+ * @author sgeor
+ */
+public class ElectricityBill {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
